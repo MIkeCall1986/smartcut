f759e0444bda183c1d956567d9f0faf9598e93d7
Santtu Keskinen <santtu.keskinen@gmail.com>
2025-08-30 16:00:12 -0700

    Windows packaging update

--- distribution/pyinstaller/windows_package.bat
+++ distribution/pyinstaller/windows_package.bat
@@ -1,12 +1,19 @@
 @echo off
 REM
 REM Navigate to the project directory; adjust this if your script is elsewhere
-cd %~dp0
+REM cd %~dp0
 
-REM Package the project with PyInstaller
-pyinstaller --distpath .\dist --workpath .\build --onefile ^
- -n smartcut -y smartcut\__main__.py
+REM Package the project with PyInstaller (single file)
+.venv\Scripts\pyinstaller --distpath .\dist --workpath .\build --onefile ^
+ -n smartcut -y --hidden-import=uuid smartcut\__main__.py
 
-..\sign.bat .\dist\smartcut.exe
+REM Package the project with PyInstaller (directory output)
+.venv\Scripts\pyinstaller --distpath .\dist --workpath .\build ^
+ -n smartcut -y --hidden-import=uuid smartcut\__main__.py
+
+REM Create zip archive of directory output
+powershell -Command "Compress-Archive -Path '.\dist\smartcut' -DestinationPath '.\dist\smartcut_win.zip' -Force"
+
+REM ..\sign.bat .\dist\smartcut.exe
 REM Pause the script to view any messages post-execution
 pause
