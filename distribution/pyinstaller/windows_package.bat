66f9ecc49d0df983f925a14587f250c1867ca5ab
Santtu Keskinen <santtu.keskinen@gmail.com>
2025-12-04 14:12:38 -0800

    Use 7z in windows packaging

--- distribution/pyinstaller/windows_package.bat
+++ distribution/pyinstaller/windows_package.bat
@@ -11,10 +11,8 @@ REM Package the project with PyInstaller (directory output)
 venv\Scripts\pyinstaller --distpath .\dist --workpath .\build ^
  -n smartcut -y --hidden-import=uuid smartcut\__main__.py
 
-REM Create zip archive of directory output
-REM Wait briefly to ensure file handles are released before zipping
-timeout /t 1 /nobreak >nul
-powershell -Command "Compress-Archive -Path '.\dist\smartcut' -DestinationPath '.\dist\smartcut_win.zip' -Force"
+REM Create zip archive of directory output using 7-Zip
+"C:\Program Files\7-Zip\7z.exe" a -tzip ".\dist\smartcut_win.zip" ".\dist\smartcut\*"
 
 REM ..\sign.bat .\dist\smartcut.exe
 REM Pause the script to view any messages post-execution
